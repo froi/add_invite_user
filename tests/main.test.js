e75beb460b6f070eafaced4185f12721295e7a53
Chris McIntosh <chocrates@github.com>
2019-12-12 15:46:08 -0600

    Adding a negative test

--- tests/main.test.js
+++ tests/main.test.js
@@ -16,22 +16,32 @@ const parsingRules = {
   }
 };
 
-let createInvitation = jest.fn().mockReturnValue(true);
-let getContents = jest.fn().mockReturnValue({
-  data: {
-    content: Buffer.from(JSON.stringify(parsingRules)).toString("base64")
-  }
-});
+let functions = {
+  createInvitation: jest.fn().mockReturnValue(true),
+  getContents: jest.fn().mockReturnValue({
+    data: {
+      content: Buffer.from(JSON.stringify(parsingRules)).toString("base64")
+    }
+  }),
+
+  setFailed: jest.fn(msg => {
+    console.log(`MOCK ERROR: ${msg}`);
+  })
+};
 
 beforeEach(() => {
+  jest.resetModules();
+  Object.entries(functions).forEach(fn => {
+    fn[1].mockClear();
+  });
   process.env.GITHUB_REPOSITORY = "testOwner/testRepo";
 
   const github = {
     repos: {
-      getContents: getContents
+      getContents: functions.getContents
     },
     orgs: {
-      createInvitation: createInvitation
+      createInvitation: functions.createInvitation
     }
   };
 
@@ -40,15 +50,12 @@ beforeEach(() => {
     repo: "repo"
   };
 
-  context.payload = {
-    issue: {
-      body: "<p>Email of Requester: user@email.com</p>"
-    }
-  };
+  setIssueBody("<p>Email of Requester: user@email.com</p>");
 
   core.debug = message => {
     console.log(`MOCK DEBUG: ${message}`);
   };
+  core.setFailed = functions.setFailed;
 
   GitHub.mockImplementation(() => github);
 });
@@ -57,14 +64,34 @@ afterEach(() => {
   process.env = OLD_ENV;
 });
 
-describe("test", () => {
-  it("tests stuff", async () => {
+let setIssueBody = body => {
+  context.payload = {
+    issue: {
+      body: body
+    }
+  };
+};
+
+describe("Main", () => {
+  it("parses the parser rules and creates an invitation with a valid body", async () => {
     core.getInput = jest
       .fn()
       .mockReturnValueOnce("/path")
       .mockReturnValueOnce("direct_member");
     await main.main();
-    expect(getContents).toHaveBeenCalledTimes(1);
-    expect(createInvitation).toHaveBeenCalledTimes(1);
+    expect(functions.getContents).toHaveBeenCalledTimes(1);
+    expect(functions.createInvitation).toHaveBeenCalledTimes(1);
+  });
+
+  it("prases the parser rules and throws an exception with an invalid bodh", async () => {
+    setIssueBody("Any test data without an email");
+    core.getInput = jest
+      .fn()
+      .mockReturnValueOnce("/path")
+      .mockReturnValueOnce("direct_member");
+    await main.main();
+    expect(functions.getContents).toHaveBeenCalledTimes(1);
+    expect(functions.createInvitation).toHaveBeenCalledTimes(0);
+    expect(functions.setFailed).toHaveBeenCalledTimes(1);
   });
 });
